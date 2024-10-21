2a50c02323862a06a4324ea3da83733ea9685a26
Chaitra <chadangat@gmail.com>
2024-10-21 20:19:44 +0530

    Added common project for cross cutting concerns, efcore nuget

--- Api/SimpleLogin/Program.cs
+++ Api/SimpleLogin/Program.cs
@@ -1,13 +1,33 @@
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// authentication
+
+// cors
+builder.Services.AddCors();
+
+// dbcontext 
+
+
+
+// logging
+builder.Services.AddLogging();
+
+
+
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
