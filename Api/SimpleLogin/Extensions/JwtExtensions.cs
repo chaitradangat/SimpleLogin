84c8b97c2fd71391e057e99de05ab18075ebc628
Chaitra <chadangat@gmail.com>
2024-11-11 02:10:47 +0530

    Adding JWT token authentication

---
+++ Api/SimpleLogin/Extensions/JwtExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace SimpleLogin.Extensions
+{
+    public static class JwtExtensions
+    {
+        public static void ConfigureJwt(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+        }
+    }
+}
