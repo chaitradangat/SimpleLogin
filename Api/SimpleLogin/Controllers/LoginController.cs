29c5e06a387d33689162ab413bf4cb93e8cdea80
Chaitra <chadangat@gmail.com>
2024-11-18 03:28:07 +0530

    Login Controller

---
+++ Api/SimpleLogin/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SimpleLogin.Controllers
+{
+    public class LoginController : ControllerBase
+    {
+        public LoginController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        { 
+            var result = new ObjectResult(1);
+
+            return Problem();
+            //return Ok();
+        } 
+
+    }
+}
